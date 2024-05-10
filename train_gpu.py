ffdaae771f740e1277e44e4b2410e00feb6c6224
Eva Micankova <81229113+xmican10@users.noreply.github.com>
2024-05-10 20:04:03 +0200

    Updating scripts

--- train_gpu.py
+++ train_gpu.py
@@ -1,3 +1,4 @@
+import os
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import torch.nn as nn
@@ -11,9 +12,8 @@ import argparse
 from tqdm import tqdm
 
 import model.unet_siamese_vgg11 as model
-
 from train.dice_loss import DiceLoss
-from mose_loader import MOSEDataset, MOSEValidDataset
+from mose_loader import MOSEDataset
 
 ## =============================================================================
 ## ------ Init CUDA
@@ -35,6 +35,13 @@ parser.add_argument('--dataset-root', type=str, default='../MOSE',
 parser.add_argument('--resume', type=str, default=None, 
                     help='Path to saved model for resuming training. '
                          'If not specified, new model will be trained.')
+parser.add_argument('--batch', type=int, default=256, 
+                    help='Batch size')
+parser.add_argument('--num_workers', type=int, default=0, 
+                    help='Number of workers')
+parser.add_argument("--lr", type=float, default=0.00001, help="Learning rate")    
+parser.add_argument("--w_decay", type=float, default=0.0001, help="Weight decay")
+
 args = parser.parse_args()
 
 ## =============================================================================
@@ -86,16 +93,16 @@ def save_checkpoint(model, filename="model.pth"):
 # --- Set training dataset params
 root_dir = args.dataset_root
 action = 'train'
-batch_size = 48
+batch_size = args.batch
 train_dataset = MOSEDataset(root_dir=root_dir, action=action)
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=args.num_workers)
 
 # --- Set validation dataset params
 root_dir = args.dataset_root
 action = 'valid'
-batch_size2 = 48
+batch_size2 = args.batch
 val_dataset = MOSEDataset(root_dir=root_dir, action=action)
-val_dataloader = DataLoader(val_dataset, batch_size=batch_size2, shuffle=True, pin_memory=True)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size2, shuffle=True, pin_memory=True, num_workers=args.num_workers)
 ## =============================================================================
 # --- Set training model params
 model = model.VGG11SiameseUnet().to(device)
@@ -105,8 +112,7 @@ if args.resume is not None:
     
 # Define loss function and optimizer
 loss_f = DiceLoss()
-#optimizer = optim.AdamW(model.parameters(), lr=1.3798384386932625e-05, weight_decay=0.0002869552100379619)
-optimizer = optim.AdamW(model.parameters(), lr=1e-05, weight_decay=1e-4)
+optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.w_decay)
 
 train_loss = []
 val_loss = []
@@ -114,16 +120,16 @@ val_loss = []
 ## =============================================================================
 # --- Training loop
 torch.backends.cudnn.benchmark = True
-for epoch in range(1, args.epochs+1):    
+for epoch in range(1, args.epochs+1):
     ## Run epoch training and validation
     train_running_loss = 0.0
     itrT = 0
-    
+
     model.train()
     loopT = tqdm(total=len(train_dataset)/batch_size, position=0, leave=True, desc=f"Epoch {epoch}/{args.epochs} [Train]")
     for batch_idx, (prev_frame1, curr_frame1, prev_annotation1, curr_annotation1) in enumerate(train_dataloader):
         itrT+=1
-        
+
         prev_frame1 = prev_frame1.to(device, non_blocking=True)
         curr_frame1 = curr_frame1.to(device, non_blocking=True)
         prev_annotation1 = prev_annotation1.to(device, non_blocking=True)
@@ -131,27 +137,22 @@ for epoch in range(1, args.epochs+1):
 
         # Zero the parameter gradients [GPU]
         optimizer.zero_grad(set_to_none=True)
-
-        # Create input tensor
-        #input_tensor1 = torch.cat((prev_frame1, curr_frame1, prev_annotation1), dim=1)
-        
         # Forward pass 1
         outputs1 = model(prev_frame1, curr_frame1, prev_annotation1)
-        
         # Compute loss
         loss = loss_f(outputs1, curr_annotation1).mean()
-
         # Backward pass and optimization
         loss.backward()
         optimizer.step()
 
         # Log statistics
         train_running_loss += loss.item()
-        
+
         loopT.update(1)
-        
+
     loopT.close()
-    
+
+
     ## Evaluate model
     if epoch % 4 == 0:
         model.eval()
@@ -161,35 +162,31 @@ for epoch in range(1, args.epochs+1):
         with torch.no_grad():
             for batch_idx, (prev_frame, curr_frame, prev_annotation, curr_annotation) in enumerate(val_dataloader):
                 itrV += 1
-                
+
                 prev_frame = prev_frame.to(device, non_blocking=True)
                 curr_frame = curr_frame.to(device, non_blocking=True)
                 prev_annotation = prev_annotation.to(device, non_blocking=True)
                 curr_annotation = curr_annotation.to(device, non_blocking=True)
-                
-                #input_tensor = torch.cat((prev_frame, curr_frame, prev_annotation), dim=1)
                 outputs = model(prev_frame, curr_frame, prev_annotation)
 
                 loss = loss_f(outputs, curr_annotation).mean()
                 val_running_loss += loss.item()
                 loopV.update(1)
-                
+        val_loss.append(val_running_loss/itrV)
+        train_loss.append(train_running_loss/itrT)
+        print('[epoch %5d] val_loss: %.3f' % (epoch, val_running_loss/itrV))
+        plot_loss(train_loss, val_loss)
         loopV.close()
-    train_loss.append(train_running_loss/itrT)
-    val_loss.append(val_running_loss/itrV)
-    
+
+
     ## Finalize epoch
     # Save checkpoint
     if (epoch % 4 == 0) and (epoch != 0):
-        filename = str(epoch)+"_"+args.output_pth
-        save_checkpoint(model, filename)
-        print(">> Checkpoint of epoch ", str(epoch), " saved.")
-    
-    # Print and plot statistics
-    print('====')
-    print('[epoch %5d] train_loss: %.3f, val_loss: %.3f' % (epoch, train_running_loss/itrT, val_running_loss/itrV))
-    print('====')
-    plot_loss(train_loss, val_loss)
+        directory, filename_with_extension = os.path.split(args.output_pth)
+        filename, extension = os.path.splitext(filename_with_extension)
+        modified_filename = f"{filename}_epoch-{epoch}{extension}"
+        file_path = os.path.join(directory, modified_filename)
 
-print('Finished Training')
-save_checkpoint(model, args.output_pth)
+        #filename = str(epoch)+"_"+args.output_pth
+        save_checkpoint(model, file_path)
+        print(">> Checkpoint of epoch ", str(epoch), " saved.")
